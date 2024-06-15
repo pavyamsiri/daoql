1439b2b49ccac4dc869863ebd5bc5c95d3db3a59
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-06-15 23:35:21 +1000

    lexer: Add whitespace skipping

--- src/lexer/tokenizer.rs
+++ src/lexer/tokenizer.rs
@@ -11,6 +11,29 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn next_token(&mut self) -> Option<Token> {
+        self.skip_whitespace();
         None
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.peek() {
+            if c.is_whitespace() {
+                self.advance();
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn advance(&mut self) {
+        self.offset += 1;
+    }
+
+    fn peek(&self) -> Option<char> {
+        self.source.chars().nth(self.offset)
+    }
+
+    fn peek_ahead(&self, next: usize) -> Option<char> {
+        self.source.chars().nth(self.offset + next)
     }
 }
