a5fab0cee73d80bfd960308fbb3959600a4f02f0
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-06-16 19:18:42 +1000

    lexer: Adjust token colors

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -27,35 +27,35 @@ pub struct Token {
 impl Token {
     pub fn begin_highlight(&self) -> &'static str {
         match self.kind {
-            TokenKind::Asterisk => "\x1b[33m",                  // Yellow
-            TokenKind::Comma => "\x1b[33m",                     // Yellow
-            TokenKind::Colon => "\x1b[33m",                     // Yellow
-            TokenKind::Semicolon => "\x1b[33m",                 // Yellow
-            TokenKind::LeftParenthesis => "\x1b[33m",           // Yellow
-            TokenKind::RightParenthesis => "\x1b[33m",          // Yellow
-            TokenKind::Percent => "\x1b[33m",                   // Yellow
-            TokenKind::Period => "\x1b[33m",                    // Yellow
-            TokenKind::Plus => "\x1b[33m",                      // Yellow
-            TokenKind::Minus => "\x1b[33m",                     // Yellow
-            TokenKind::Solidus => "\x1b[33m",                   // Yellow
-            TokenKind::LessThan => "\x1b[33m",                  // Yellow
-            TokenKind::GreaterThan => "\x1b[33m",               // Yellow
-            TokenKind::Equals => "\x1b[33m",                    // Yellow
-            TokenKind::Underscore => "\x1b[33m",                // Yellow
-            TokenKind::Ampersand => "\x1b[33m",                 // Yellow
-            TokenKind::QuestionMark => "\x1b[33m",              // Yellow
-            TokenKind::Circumflex => "\x1b[33m",                // Yellow
-            TokenKind::Tilde => "\x1b[33m",                     // Yellow
-            TokenKind::VerticalBar => "\x1b[33m",               // Yellow
-            TokenKind::Keyword(_) => "\x1b[34m",                // Blue
-            TokenKind::Identifier => "\x1b[32m",                // Green
-            TokenKind::SingleQuotedStringLiteral => "\x1b[35m", // Magenta
-            TokenKind::DoubleQuotedStringLiteral => "\x1b[35m", // Magenta
-            TokenKind::IntegerLiteral => "\x1b[36m",            // Cyan
-            TokenKind::DecimalLiteral => "\x1b[36m",            // Cyan
-            TokenKind::HexadecimalLiteral => "\x1b[36m",        // Cyan
-            TokenKind::ExponentLiteral => "\x1b[36m",           // Cyan
-            TokenKind::InlineComment => "\x1b[90m",             // Bright Black (Gray)
+            TokenKind::Asterisk => "\x1b[33m",                    // Yellow
+            TokenKind::Comma => "\x1b[33m",                       // Yellow
+            TokenKind::Colon => "\x1b[33m",                       // Yellow
+            TokenKind::Semicolon => "\x1b[33m",                   // Yellow
+            TokenKind::LeftParenthesis => "\x1b[33m",             // Yellow
+            TokenKind::RightParenthesis => "\x1b[33m",            // Yellow
+            TokenKind::Percent => "\x1b[33m",                     // Yellow
+            TokenKind::Period => "\x1b[33m",                      // Yellow
+            TokenKind::Plus => "\x1b[33m",                        // Yellow
+            TokenKind::Minus => "\x1b[33m",                       // Yellow
+            TokenKind::Solidus => "\x1b[33m",                     // Yellow
+            TokenKind::LessThan => "\x1b[33m",                    // Yellow
+            TokenKind::GreaterThan => "\x1b[33m",                 // Yellow
+            TokenKind::Equals => "\x1b[33m",                      // Yellow
+            TokenKind::Underscore => "\x1b[33m",                  // Yellow
+            TokenKind::Ampersand => "\x1b[33m",                   // Yellow
+            TokenKind::QuestionMark => "\x1b[33m",                // Yellow
+            TokenKind::Circumflex => "\x1b[33m",                  // Yellow
+            TokenKind::Tilde => "\x1b[33m",                       // Yellow
+            TokenKind::VerticalBar => "\x1b[33m",                 // Yellow
+            TokenKind::Keyword(_) => "\x1b[1;31m",                // Bright Red
+            TokenKind::Identifier => "\x1b[1;34m",                // Bright Blue
+            TokenKind::SingleQuotedStringLiteral => "\x1b[1;35m", // Magenta
+            TokenKind::DoubleQuotedStringLiteral => "\x1b[1;35m", // Magenta
+            TokenKind::IntegerLiteral => "\x1b[1;36m",            // Cyan
+            TokenKind::DecimalLiteral => "\x1b[1;36m",            // Cyan
+            TokenKind::HexadecimalLiteral => "\x1b[1;36m",        // Cyan
+            TokenKind::ExponentLiteral => "\x1b[1;36m",           // Cyan
+            TokenKind::InlineComment => "\x1b[90m",               // Bright Black (Gray)
         }
     }
 
